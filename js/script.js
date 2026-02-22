fbf6062abb322029609235729aa2288d3499c94c
saynika <callmeyoulike@gmail.com>
2026-02-22 20:31:09 +0100

    заглушка на кнопки хедера

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 <script src="js/script.js"></script>
 
+// ===== HEADER BUTTONS =====
+document.querySelectorAll('.call-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert("Ruft die Firma an!");
+    });
+});
+
+document.querySelectorAll('.write-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert("Öffnet das Kontaktformular!");
+    });
+});
