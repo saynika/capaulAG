90778b4557093406f2fd177dc4335ea4d4dddebd
saynika <callmeyoulike@gmail.com>
2026-02-22 21:40:55 +0100

    фіксік

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
+// js/script.js
 console.log("JS підключено ✅");
-<link rel="stylesheet" href="./css/style.css">
-    
-const modal = document.getElementById("contactModal");
-const openModal = document.getElementById("openModal");
-const openModalFooter = document.getElementById("openModalFooter");
-const form = document.getElementById("contactForm");
 
-openModal.addEventListener("click", () => {
-    modal.style.display = "flex";
-});
 
-openModalFooter.addEventListener("click", () => {
-    modal.style.display = "flex";
-});
+// Перевіримо, що елементи існують
+if(openModal) {
+    openModal.addEventListener("click", () => {
+        modal.style.display = "flex";
+    });
+}
 
-window.addEventListener("click", (e) => {
-    if (e.target === modal) {
+if(openModalFooter) {
+    openModalFooter.addEventListener("click", () => {
+        modal.style.display = "flex";
+    });
+}
+
+if(modal) {
+    window.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+}
+
+if(form) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
         modal.style.display = "none";
-    }
-});
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    modal.style.display = "none";
-    alert("Ihre Nachricht wurde erfolgreich gesendet");
-});
-
-<script src="./js/script.js"></script>
+        alert("Ihre Nachricht wurde erfolgreich gesendet");
+    });
+}
